f5a43b55ebb9f5e806bf68607967cc352693adc7
Janielly Lorene <janilorene@gmail.com>
2025-06-22 21:09:13 -0300

    Alterações para fazer o site funcional

--- api/index.js
+++ api/index.js
@@ -1,15 +1,11 @@
-// api/jsonserver.js
 const jsonServer = require('json-server');
 const path = require('path');
-
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(process.cwd(), 'db.json'));
+const router = jsonServer.router(path.join(__dirname, '..', 'db', 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(router);
 
-module.exports = (req, res) => {
-  server(req, res);
-};
+module.exports = server;
