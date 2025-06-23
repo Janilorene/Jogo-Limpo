58d9486b3da7e82d3b3eaa41ec3ae75fb0c96946
Janielly Lorene <janilorene@gmail.com>
2025-06-22 22:14:35 -0300

    tentativa 6

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
 const middlewares = jsonServer.defaults();
+const path = require('path');
+const cors = require('cors');
 
+// Middleware CORS manual
 server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // permite qualquer origem
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') {
@@ -14,15 +17,13 @@ server.use((req, res, next) => {
   next();
 });
 
-server.use(middlewares);
-const cors = require('cors');
 server.use(cors({
-  origin: '*', // ou substitua por sua URL: 'https://seu-frontend.vercel.app'
-  methods: ['GET','POST','PATCH','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization'],
-  credentials: true
+  origin: '*',
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+server.use(middlewares);
 server.use(router);
 
 module.exports = server;
