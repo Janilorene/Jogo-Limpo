4415751c0bd02bd13afac0723dff3109baf2716d
Janielly Lorene <janilorene@gmail.com>
2025-06-22 21:26:30 -0300

    tentativa 2

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
-// api/server.js
 const jsonServer = require('json-server');
 const path = require('path');
+const cors = require('cors');
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
 const middlewares = jsonServer.defaults();
 
+server.use(cors()); 
 server.use(middlewares);
 server.use(router);
 
